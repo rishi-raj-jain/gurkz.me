7476f61320d61f2ac3306ab6315d2127cf6616a5
Gurkan <hello@gurkz.me>
2024-01-16 12:47:21 +0100

    update socials page, bad styling

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,27 @@
+type SocialAccount = {
+    platform: string
+} & {
+    username: string
+} | {
+    link: string
+}
+
 export const SITE_TITLE = "gurkan -";
+export const SOCIAL_ACCOUNTS: SocialAccount[] = [
+    {
+        platform: "twitter",
+        link: "https://twitter.com/@thatgurkangurk"
+    },
+    {
+        platform: "github",
+        link: "https://github.com/thatgurkangurk"
+    },
+    {
+        platform: "reddit",
+        link: "https://reddit.com/u/thatgurkangurk"
+    },
+    {
+        platform: "discord",
+        username: "@thatgurkangurk"
+    }
+]
