53e5faead0ea6164cfd1415df41c2f530986c41f
Gurkan <hello@gurkz.me>
2024-01-23 10:48:36 +0100

    card icon

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,11 +1,13 @@
-import type { JSXElement } from "solid-js";
+import { Switch, type JSXElement, Match } from "solid-js";
 import { cva, type VariantProps } from "cva";
 import { twMerge } from "tailwind-merge";
+import { BiRegularError, BiRegularErrorAlt } from "solid-icons/bi";
+import { BsCheckLg } from "solid-icons/bs";
 
-const cardVariants = cva("my-2 px-4 rounded-md p-2 text-center", {
+const cardVariants = cva("my-2 px-4 rounded-md p-2 flex gap-2 items-center", {
   variants: {
     type: {
-      success: "bg-teal-400",
+      success: "bg-teal-600",
       warning: "bg-orange-500",
       error: "bg-red-600 font-bold",
     },
@@ -22,7 +24,20 @@ type Props = {
 function Card(props: Props) {
   return (
     <div class={twMerge(cardVariants({ type: props.type }))}>
-      {props.children}
+      <div>
+        <Switch fallback={<></>}>
+          <Match when={props.type === "success"}>
+            <BsCheckLg size={32} />
+          </Match>
+          <Match when={props.type === "warning"}>
+            <BiRegularError size={32} />
+          </Match>
+          <Match when={props.type === "error"}>
+            <BiRegularErrorAlt size={32} />
+          </Match>
+        </Switch>
+      </div>
+      <div class="pr-4">{props.children}</div>
     </div>
   );
 }
